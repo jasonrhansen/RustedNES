eebc301b09b14d5fc6ba1d1b6d788d757bf3b869
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2018-01-27 15:40:09 -0700

    Implement mapper 7

--- sadnes-core/src/mapper/mapper7.rs
+++ sadnes-core/src/mapper/mapper7.rs
@@ -1,26 +1,25 @@
-use cartridge::{Cartridge, PRG_ROM_BANK_SIZE};
+use cartridge::{Cartridge, Mirroring};
 use mapper::Mapper;
 use memory::Memory;
 use ppu::{self, Ppu, Vram};
 use cpu::{Cpu, Interrupt};
 
 
-pub struct Mapper2 {
+pub struct Mapper7 {
     cartridge: Box<Cartridge>,
-    switchable_bank: u8,
+    prg_rom_bank: u8,
 }
 
-impl Mapper2 {
+impl Mapper7 {
     pub fn new(cartridge: Box<Cartridge>) -> Self {
-        Mapper2 {
+        Mapper7 {
             cartridge,
-            switchable_bank: 0,
+            prg_rom_bank: 0,
         }
     }
 
     fn prg_rom_address(bank: u8, address: u16) -> usize {
-        (bank as usize *  PRG_ROM_BANK_SIZE as usize) |
-            (address as usize & (PRG_ROM_BANK_SIZE as usize - 1))
+        (bank as usize * 0x8000 as usize) | (address as usize & 0x7FFF)
     }
 
     fn mirror_address(&self, address: u16) -> u16 {
@@ -28,18 +27,12 @@ impl Mapper2 {
     }
 
     fn read_prg_rom(&mut self, address: u16) -> u8 {
-        let bank = if address < 0xC000 {
-            self.switchable_bank
-        } else {
-            self.cartridge.prg_rom_num_banks - 1
-        };
-
-        let rom_addr = Mapper2::prg_rom_address(bank, address);
+        let rom_addr = Mapper7::prg_rom_address(self.prg_rom_bank, address);
         self.cartridge.prg_rom[rom_addr as usize]
     }
 }
 
-impl Mapper for Mapper2 {
+impl Mapper for Mapper7 {
     fn prg_read_byte(&mut self, address: u16) -> u8 {
         if address < 0x6000 {
             0
@@ -50,7 +43,12 @@ impl Mapper for Mapper2 {
 
     fn prg_write_byte(&mut self, address: u16, value: u8) {
         if address >= 0x8000 {
-            self.switchable_bank = value;
+            self.prg_rom_bank = value & 0x07;
+            self.cartridge.mirroring = if value & 0x10 == 0 {
+                Mirroring::OneScreenLower
+            } else {
+                Mirroring::OneScreenUpper
+            };
         }
     }
 
--- sadnes-core/src/mapper/mod.rs
+++ sadnes-core/src/mapper/mod.rs
@@ -2,11 +2,13 @@ mod mapper0;
 mod mapper1;
 mod mapper2;
 mod mapper4;
+mod mapper7;
 
 use self::mapper0::Mapper0;
 use self::mapper1::Mapper1;
 use self::mapper2::Mapper2;
 use self::mapper4::Mapper4;
+use self::mapper7::Mapper7;
 use super::ppu::Vram;
 use super::cartridge::Cartridge;
 use super::cpu::Cpu;
@@ -17,7 +19,7 @@ pub trait Mapper {
     fn prg_write_byte(&mut self, address: u16, value: u8);
     fn ppu_read_byte(&mut self, vram: &mut Vram, address: u16) -> u8;
     fn ppu_write_byte(&mut self, vram: &mut Vram, address: u16, value: u8);
-    fn step(&mut self, cpu: &mut Cpu, ppu: &Ppu) {}
+    fn step(&mut self, _cpu: &mut Cpu, _ppu: &Ppu) {}
 }
 
 pub fn create_mapper(cartridge: Box<Cartridge>) -> Box<Mapper> {
@@ -26,6 +28,7 @@ pub fn create_mapper(cartridge: Box<Cartridge>) -> Box<Mapper> {
         1 => Box::new(Mapper1::new(cartridge)),
         2 => Box::new(Mapper2::new(cartridge)),
         4 => Box::new(Mapper4::new(cartridge)),
+        7 => Box::new(Mapper7::new(cartridge)),
         _ => panic!("Unsupported mapper number: {}", cartridge.mapper)
     }
 }
