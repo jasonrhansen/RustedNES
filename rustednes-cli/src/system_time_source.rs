72480d43f4d9e0c79adbde37327d6964e548224f
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-12-08 13:06:06 -0700

    Update dependencies

--- rustednes-cli/src/system_time_source.rs
+++ rustednes-cli/src/system_time_source.rs
@@ -6,6 +6,6 @@ pub struct SystemTimeSource {}
 
 impl TimeSource for SystemTimeSource {
     fn time_ns(&self) -> u64 {
-        (OffsetDateTime::now_utc() - OffsetDateTime::unix_epoch()).whole_nanoseconds() as u64
+        (OffsetDateTime::now_utc() - OffsetDateTime::UNIX_EPOCH).whole_nanoseconds() as u64
     }
 }
