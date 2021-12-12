4035522a082ab28a50f7f161701f5013b95342da
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-12-12 13:48:37 -0700

    SDL2: Refactor

--- rustednes-sdl/src/emulator.rs
+++ rustednes-sdl/src/emulator.rs
@@ -6,13 +6,13 @@ use rustednes_core::ppu::{SCREEN_HEIGHT, SCREEN_WIDTH};
 use rustednes_core::sink::*;
 use rustednes_core::time_source::TimeSource;
 
-use sdl2::event::Event;
+use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::{KeyboardState, Keycode, Mod, Scancode};
 use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::rect::Rect;
-use sdl2::render::Canvas;
+use sdl2::render::{Canvas, Texture};
 use sdl2::video::{FullscreenType, Window};
-use sdl2::Sdl;
+use sdl2::{EventPump, Sdl};
 
 use std::thread;
 use std::time::Duration;
@@ -25,6 +25,7 @@ pub struct Emulator<A: AudioSink, T: TimeSource> {
 
     sdl_context: Sdl,
     canvas: Canvas<Window>,
+
     audio_frame_sink: A,
     time_source: T,
     start_time_ns: u64,
@@ -53,12 +54,16 @@ where
     {
         let video_subsystem = sdl_context.video().unwrap();
 
+        let debug_scale = 4;
         let debug_window = video_subsystem
-            .window("Debug", 256, 128)
+            .window("Debug", 256 * debug_scale, 128 * debug_scale)
             .hidden()
             .build()
             .unwrap();
         let mut debug_canvas = debug_window.into_canvas().build().unwrap();
+        debug_canvas
+            .set_scale(debug_scale as f32, debug_scale as f32)
+            .unwrap();
         debug_canvas.set_draw_color(Color::RGB(0, 0, 0));
         debug_canvas.clear();
         debug_canvas.present();
@@ -86,6 +91,7 @@ where
 
             sdl_context,
             canvas,
+
             audio_frame_sink,
             time_source,
             start_time_ns: 0,
@@ -99,6 +105,7 @@ where
     }
 
     pub fn run(&mut self) {
+        // Create a texture we can use to render emulation frames.
         let mut pixel_buffer = vec![0; SCREEN_WIDTH * SCREEN_HEIGHT];
         let texture_creator = self.canvas.texture_creator();
         let mut texture = texture_creator
@@ -112,48 +119,16 @@ where
 
         self.start_time_ns = self.time_source.time_ns();
 
+        // Main event/emulation loop
         let mut event_pump = self.sdl_context.event_pump().unwrap();
-        'running: loop {
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit { .. }
-                    | Event::KeyDown {
-                        keycode: Some(Keycode::Escape),
-                        ..
-                    } => break 'running,
-
-                    Event::KeyDown {
-                        keycode: Some(Keycode::F11),
-                        ..
-                    }
-                    | Event::KeyDown {
-                        keycode: Some(Keycode::F),
-                        keymod: Mod::LCTRLMOD | Mod::RCTRLMOD,
-                        ..
-                    }
-                    | Event::KeyDown {
-                        keycode: Some(Keycode::Return),
-                        keymod: Mod::LALTMOD | Mod::RALTMOD,
-                        ..
-                    } => {
-                        self.toggle_fullscreen();
-                    }
-                    Event::KeyDown {
-                        keycode: Some(Keycode::F5),
-                        ..
-                    } => {
-                        self.toggle_debugging();
-                    }
-                    Event::Window { .. } => {
-                        self.start_time_ns =
-                            self.time_source.time_ns() - (self.emulated_cycles * CPU_CYCLE_TIME_NS);
-                    }
-                    _ => {}
-                }
+        loop {
+            if !self.handle_events(&mut event_pump) {
+                break;
             }
 
+            // Run enough emulator cycles to catch up with the time that has passed since the
+            // previous loop iteration.
             let mut video_frame_sink = Xrgb8888VideoSink::new(&mut pixel_buffer);
-
             let target_time_ns = self.time_source.time_ns() - self.start_time_ns;
             let target_cycles = target_time_ns / CPU_CYCLE_TIME_NS;
             while self.emulated_cycles < target_cycles {
@@ -162,87 +137,18 @@ where
             }
 
             if self.debugging {
-                self.draw_debug_window();
+                self.render_debug_window();
             }
 
             if video_frame_sink.frame_written() {
-                texture
-                    .update(
-                        None,
-                        unsafe { std::mem::transmute(pixel_buffer.as_slice()) },
-                        SCREEN_WIDTH * 4,
-                    )
-                    .unwrap();
-
-                // Maintain aspect ratio when resizing window.
-                let (target_width, target_height) = self.canvas.window().drawable_size();
-                let target_ratio = target_width as f32 / target_height as f32;
-                let dest_rect = if SCREEN_RATIO <= target_ratio {
-                    let width =
-                        (SCREEN_WIDTH as f32 * target_height as f32 / SCREEN_HEIGHT as f32) as u32;
-                    Rect::new((target_width - width) as i32 / 2, 0, width, target_height)
-                } else {
-                    let height =
-                        (SCREEN_HEIGHT as f32 * target_width as f32 / SCREEN_WIDTH as f32) as u32;
-                    Rect::new(0, (target_height - height) as i32 / 2, target_width, height)
-                };
-
-                self.canvas.clear();
-                self.canvas.copy(&texture, None, Some(dest_rect)).unwrap();
-                self.canvas.present();
-
+                self.render_frame(pixel_buffer.as_ref(), &mut texture);
                 self.update_gamepad(event_pump.keyboard_state());
             }
 
-            thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+            thread::sleep(Duration::new(0, 1_000_000_000 / 60));
         }
 
         self.cleanup();
-    }
-
-    fn draw_debug_window(&mut self) {
-        // DCBA98 76543210
-        // ---------------
-        // 0HRRRR CCCCPTTT
-        // |||||| |||||+++- T: Fine Y offset, the row number within a tile
-        // |||||| ||||+---- P: Bit plane (0: "lower"; 1: "upper")
-        // |||||| ++++----- C: Tile column
-        // ||++++---------- R: Tile row
-        // |+-------------- H: Half of sprite table (0: "left"; 1: "right")
-        // +--------------- 0: Pattern table is at $0000-$1FFF
-        self.debug_canvas.clear();
-        let mut mapper = self.nes.interconnect.mapper.borrow_mut();
-        for half in 0..=1 {
-            for row in 0..16 {
-                for y in 0..8 {
-                    let point_y = (row * 8 + y) as i32;
-                    for col in 0..16 {
-                        let tile_x = (half * 128 + col * 8) as i32;
-                        let lower_addr: u16 = half << 12 | row << 8 | col << 4 | y;
-                        let upper_addr = lower_addr | 0x08;
-                        let lower_byte = mapper.chr_read_byte(lower_addr);
-                        let upper_byte = mapper.chr_read_byte(upper_addr);
-
-                        for bit in 0..8 {
-                            let pixel = ((lower_byte & (1 << bit)) >> bit)
-                                | ((upper_byte & (1 << bit)) >> (bit - 1));
-                            let shade = match pixel {
-                                3 => 255,
-                                2 => 255 / 3 * 2,
-                                1 => 255 / 3,
-                                _ => 0,
-                            };
-                            self.debug_canvas
-                                .set_draw_color(Color::RGB(shade, shade, shade));
-                            self.debug_canvas
-                                .draw_point((tile_x + (7 - bit) as i32, point_y))
-                                .unwrap();
-                        }
-                    }
-                }
-            }
-        }
-        self.debug_canvas.present();
     }
 
     fn step<V: VideoSink>(&mut self, video_frame_sink: &mut V) -> (u32, bool) {
@@ -252,6 +158,58 @@ where
         self.emulated_cycles += cycles as u64;
 
         (cycles, trigger_watchpoint)
+    }
+
+    /// Returns false to signal to end emulation.
+    fn handle_events(&mut self, events: &mut EventPump) -> bool {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => return false,
+                Event::Window {
+                    window_id,
+                    win_event: WindowEvent::Close,
+                    ..
+                } => {
+                    if window_id == self.canvas.window().id() {
+                        return false;
+                    } else if window_id == self.debug_canvas.window().id() {
+                        self.toggle_debugging();
+                    }
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::F11),
+                    ..
+                }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::F),
+                    keymod: Mod::LCTRLMOD | Mod::RCTRLMOD,
+                    ..
+                }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Return),
+                    keymod: Mod::LALTMOD | Mod::RALTMOD,
+                    ..
+                } => {
+                    self.toggle_fullscreen();
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::F5),
+                    ..
+                } => {
+                    self.toggle_debugging();
+                }
+                Event::Window { .. } => {
+                    self.start_time_ns =
+                        self.time_source.time_ns() - (self.emulated_cycles * CPU_CYCLE_TIME_NS);
+                }
+                _ => {}
+            }
+        }
+        return true;
     }
 
     fn update_gamepad(&mut self, keyboard_state: KeyboardState) {
@@ -311,6 +269,79 @@ where
         }
 
         self.debugging = !self.debugging;
+    }
+
+    /// Render a frame of emulation.
+    fn render_frame(&mut self, pixel_buffer: &[u32], texture: &mut Texture) {
+        texture
+            .update(
+                None,
+                unsafe { std::mem::transmute(pixel_buffer) },
+                SCREEN_WIDTH * 4,
+            )
+            .unwrap();
+
+        // Maintain aspect ratio when resizing window.
+        let (target_width, target_height) = self.canvas.window().drawable_size();
+        let target_ratio = target_width as f32 / target_height as f32;
+        let dest_rect = if SCREEN_RATIO <= target_ratio {
+            let width = (SCREEN_WIDTH as f32 * target_height as f32 / SCREEN_HEIGHT as f32) as u32;
+            Rect::new((target_width - width) as i32 / 2, 0, width, target_height)
+        } else {
+            let height = (SCREEN_HEIGHT as f32 * target_width as f32 / SCREEN_WIDTH as f32) as u32;
+            Rect::new(0, (target_height - height) as i32 / 2, target_width, height)
+        };
+
+        self.canvas.clear();
+        self.canvas.copy(&texture, None, Some(dest_rect)).unwrap();
+        self.canvas.present();
+    }
+
+    /// Render debug info
+    fn render_debug_window(&mut self) {
+        // DCBA98 76543210
+        // ---------------
+        // 0HRRRR CCCCPTTT
+        // |||||| |||||+++- T: Fine Y offset, the row number within a tile
+        // |||||| ||||+---- P: Bit plane (0: "lower"; 1: "upper")
+        // |||||| ++++----- C: Tile column
+        // ||++++---------- R: Tile row
+        // |+-------------- H: Half of sprite table (0: "left"; 1: "right")
+        // +--------------- 0: Pattern table is at $0000-$1FFF
+        self.debug_canvas.clear();
+        let mut mapper = self.nes.interconnect.mapper.borrow_mut();
+        for half in 0..=1 {
+            for row in 0..16 {
+                for y in 0..8 {
+                    let point_y = (row * 8 + y) as i32;
+                    for col in 0..16 {
+                        let tile_x = (half * 128 + col * 8) as i32;
+                        let lower_addr: u16 = half << 12 | row << 8 | col << 4 | y;
+                        let upper_addr = lower_addr | 0x08;
+                        let lower_byte = mapper.chr_read_byte(lower_addr);
+                        let upper_byte = mapper.chr_read_byte(upper_addr);
+
+                        for bit in 0..8 {
+                            let pixel = ((lower_byte & (1 << bit)) >> bit)
+                                | ((upper_byte & (1 << bit)) >> (bit - 1));
+                            let shade = match pixel {
+                                3 => 255,
+                                2 => 255 / 3 * 2,
+                                1 => 255 / 3,
+                                _ => 0,
+                            };
+                            self.debug_canvas
+                                .set_draw_color(Color::RGB(shade, shade, shade));
+                            self.debug_canvas
+                                .draw_point((tile_x + (7 - bit) as i32, point_y))
+                                .unwrap();
+                        }
+                    }
+                }
+            }
+        }
+
+        self.debug_canvas.present();
     }
 
     fn cleanup(&mut self) {
